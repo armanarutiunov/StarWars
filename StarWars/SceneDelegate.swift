cfb5143b99cce15bbbb640988726113cfef05543
arman <arman.arutiunov@gmail.com>
2023-08-19 19:10:10 +0100

    🔧 Add window?.makeKeyAndVisible() in SceneDelegate

--- StarWars/SceneDelegate.swift
+++ StarWars/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = FilmographySplitViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
