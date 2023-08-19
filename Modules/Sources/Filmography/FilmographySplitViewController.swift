f1c9a575b373c22ffbf78348319fcf91de5e9288
arman <arman.arutiunov@gmail.com>
2023-08-19 19:12:42 +0100

    ✨ Make all columns visible at all times

--- Modules/Sources/Filmography/FilmographySplitViewController.swift
+++ Modules/Sources/Filmography/FilmographySplitViewController.swift
@@ -35,5 +35,7 @@ public final class FilmographySplitViewController: UISplitViewController {
             UINavigationController(rootViewController: charactersViewController),
             UINavigationController(rootViewController: characterDetailsViewController)
         ]
+
+        preferredDisplayMode = .twoBesideSecondary
     }
 }
