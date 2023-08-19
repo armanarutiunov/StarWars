14d8d1e928e10c0cf3e18c9c4880b653a450b02f
arman <arman.arutiunov@gmail.com>
2023-08-19 18:06:55 +0100

    🔧 Fix how ID is setup in Character (same reason as in Film)

--- Modules/Sources/FilmographyManager/Character/Character.swift
+++ Modules/Sources/FilmographyManager/Character/Character.swift
@@ -20,13 +20,12 @@ public struct Character: Identifiable, Decodable {
         case eyeColor = "eye_color"
         case birthYear = "birth_year"
         case gender
+        case url
     }
 
     // MARK: - Properties
 
-    public var id: Int {
-        hashValue
-    }
+    public let id: String
 
     public let name: String
 
@@ -46,7 +45,8 @@ public struct Character: Identifiable, Decodable {
 
     // MARK: - Life Cycle
 
-    init(name: String, height: String, mass: String, hairColor: String, skinColor: String, eyeColor: String, birthYear: String, gender: String) {
+    init(id: String, name: String, height: String, mass: String, hairColor: String, skinColor: String, eyeColor: String, birthYear: String, gender: String) {
+        self.id = id
         self.name = name
         self.height = height
         self.mass = mass
@@ -55,6 +55,19 @@ public struct Character: Identifiable, Decodable {
         self.eyeColor = eyeColor
         self.birthYear = birthYear
         self.gender = gender
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(URL.self, forKey: .url).lastPathComponent
+        self.name = try container.decode(String.self, forKey: .name)
+        self.height = try container.decode(String.self, forKey: .height)
+        self.mass = try container.decode(String.self, forKey: .mass)
+        self.hairColor = try container.decode(String.self, forKey: .hairColor)
+        self.skinColor = try container.decode(String.self, forKey: .skinColor)
+        self.eyeColor = try container.decode(String.self, forKey: .eyeColor)
+        self.birthYear = try container.decode(String.self, forKey: .birthYear)
+        self.gender = try container.decode(String.self, forKey: .gender)
     }
 }
 
