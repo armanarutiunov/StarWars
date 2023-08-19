6c6850a781956ac7f49663b08c479a1a9ced9f75
arman <arman.arutiunov@gmail.com>
2023-08-19 16:04:58 +0100

    ✨ Setup FilmographySplitViewController as the root VC

--- Modules/Sources/Filmography/FilmographySplitViewController.swift
+++ Modules/Sources/Filmography/FilmographySplitViewController.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 public final class FilmographySplitViewController: UISplitViewController {
-    
+
+    // MARK: - Life Cycle
+
+    public init() {
+        super.init(style: .tripleColumn)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
