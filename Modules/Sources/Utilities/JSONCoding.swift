9d23146c5e1c42a88ec534997fe055c2d2513d2d
arman <arman.arutiunov@gmail.com>
2023-08-20 20:11:24 +0100

    ✨ Add encodeToJSON() in JSONCoding utilities

--- Modules/Sources/Utilities/JSONCoding.swift
+++ Modules/Sources/Utilities/JSONCoding.swift
@@ -1,5 +1,5 @@
 //
-//  JSONDecoding.swift
+//  JSONCoding.swift
 //  
 //
 //  Created by Arman Arutiunov on 20/08/2023.
@@ -17,4 +17,10 @@ public func decodeFromJSON<T: Decodable>(json: [String: Any?]) -> T? {
         print("Unable to parse: \(error)")
         return nil
     }
+}
+
+public func encodeToJSON(object: some Encodable) -> [String: Any]? {
+    let encoder = JSONEncoder()
+    let data = try! encoder.encode(object)
+    return try? JSONSerialization.jsonObject(with: data) as? [String: Any]
 }
