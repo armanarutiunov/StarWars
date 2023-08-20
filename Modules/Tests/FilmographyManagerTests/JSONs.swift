e898b2c111130b60edbfad09c0477438434f2042
arman <arman.arutiunov@gmail.com>
2023-08-20 13:39:52 +0100

    ✨ Create a Character JSON for decode testing

---
+++ Modules/Tests/FilmographyManagerTests/JSONs.swift
@@ -0,0 +1,21 @@
+//
+//  JSONs.swift
+//  
+//
+//  Created by Arman Arutiunov on 20/08/2023.
+//
+
+@testable import FilmographyManager
+import Foundation
+
+let CharacterJSON: [String: Any] = [
+    Character.CodingKeys.name.rawValue: "Luke Skywalker",
+    Character.CodingKeys.height.rawValue: "172",
+    Character.CodingKeys.mass.rawValue: "77",
+    Character.CodingKeys.hairColor.rawValue: "blond",
+    Character.CodingKeys.skinColor.rawValue: "fair",
+    Character.CodingKeys.eyeColor.rawValue: "blue",
+    Character.CodingKeys.birthYear.rawValue: "19BBY",
+    Character.CodingKeys.gender.rawValue: "male",
+    Character.CodingKeys.url.rawValue: "https://swapi.dev/api/people/1/"
+]
