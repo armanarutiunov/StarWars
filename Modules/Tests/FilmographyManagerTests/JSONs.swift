eb806a7c9305fdebb8d4ea1b8ad79691fc867c9b
arman <arman.arutiunov@gmail.com>
2023-08-20 13:50:06 +0100

    ✨ Create a Film JSON for decode testing

--- Modules/Tests/FilmographyManagerTests/JSONs.swift
+++ Modules/Tests/FilmographyManagerTests/JSONs.swift
@@ -19,3 +19,26 @@ let CharacterJSON: [String: Any] = [
     Character.CodingKeys.gender.rawValue: "male",
     Character.CodingKeys.url.rawValue: "https://swapi.dev/api/people/1/"
 ]
+
+let FilmJSON: [String: Any] = [
+    Film.CodingKeys.title.rawValue: "The Empire Strikes Back",
+    Film.CodingKeys.url.rawValue: "https://swapi.dev/api/films/2/",
+    Film.CodingKeys.characterURLs.rawValue: [
+        "https://swapi.dev/api/people/1/",
+        "https://swapi.dev/api/people/2/",
+        "https://swapi.dev/api/people/3/",
+        "https://swapi.dev/api/people/4/",
+        "https://swapi.dev/api/people/5/",
+        "https://swapi.dev/api/people/10/",
+        "https://swapi.dev/api/people/13/",
+        "https://swapi.dev/api/people/14/",
+        "https://swapi.dev/api/people/18/",
+        "https://swapi.dev/api/people/20/",
+        "https://swapi.dev/api/people/21/",
+        "https://swapi.dev/api/people/22/",
+        "https://swapi.dev/api/people/23/",
+        "https://swapi.dev/api/people/24/",
+        "https://swapi.dev/api/people/25/",
+        "https://swapi.dev/api/people/26/"
+    ]
+]
